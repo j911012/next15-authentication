9db30c96f32b1184c4be7bbb7fcbd5565386936a
jun <juntetsu.1012@gmail.com>
2025-04-05 17:02:08 +0900

    セッションとCookieの設定(Lucia Auth)

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,8 @@
-import { Lucia } from "lucia";
+import { Lucia, User } from "lucia";
 import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
 
 import db from "@/lib/db";
+import { cookies } from "next/headers";
 
 const adapter = new BetterSqlite3Adapter(db, {
   user: "users",
@@ -18,3 +19,15 @@ export const lucia = new Lucia(adapter, {
 });
 
 export type Auth = typeof lucia;
+
+export async function createAuthSession(userId: User["id"]): Promise<void> {
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+
+  const cookieStore = await cookies();
+  cookieStore.set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+}
